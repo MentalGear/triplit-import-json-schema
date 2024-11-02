f4909e10c9813b9c1a3da25029bc2674f35128ad
mentalgear <2837147+MentalGear@users.noreply.github.com>
2024-11-02 22:18:02 +0100

    chore: comment out valibot

--- packages/db/src/schema/super-schema/adapters/valibot.ts
+++ packages/db/src/schema/super-schema/adapters/valibot.ts
@@ -1,40 +1,41 @@
-import * as v from 'valibot';
-import { toJsonSchema } from '@valibot/to-json-schema';
-import { ValidationLibAdapter } from './ValidationLibAdapter';
+// import * as v from 'valibot';
+// import { toJsonSchema } from '@valibot/to-json-schema';
+// import { ValidationLibAdapter } from './ValidationLibAdapter.js';
 
-export const valibotAdapter: ValidationLibAdapter = {
-  idMapping: {
-    nanoid: function () {
-      // TODO: fill in validation lib function to generate nanoid ?
-      return function () {};
-    },
-  },
-  // TODO: make valibot
-  typeMapping: {
-    Boolean: 'ZodBoolean',
-    String: 'ZodString',
-    Number: 'ZodNumber',
-    Date: 'ZodDate',
-    Set: 'ZodSet',
-    Nullable: 'ZodNullable',
-    Optional: 'ZodOptional',
-    Record: 'ZodRecord',
-    // Object: 'ZodObject',
-  },
-  // ========================================================================
-  wrapInContainer: function (obj: any) {
-    return v.object(obj);
-  },
-  isOwnContainer: function (obj: any) {
-    const isValibotObjectType =
-      obj?.expects === 'Object' && obj?.entries != null;
-    return isValibotObjectType;
-  },
-  isOwnType: function (obj: any) {
-    const isValibotType = obj?.async != null && obj?._run != null;
-    return isValibotType;
-  },
-  jsonSchemaFrom: function (validationLibSchema: any) {
-    return toJsonSchema(validationLibSchema);
-  },
-};
+// export const valibotAdapter: ValidationLibAdapter = {
+//   // idMapping: {
+//   //   nanoid: function () {
+//   //     // TODO: fill in validation lib function to generate nanoid ?
+//   //     return function () {};
+//   //   },
+//   // },
+//   // TODO: make valibot
+//   typeMapping: {
+//     Boolean: 'ZodBoolean',
+//     String: 'ZodString',
+//     Number: 'ZodNumber',
+//     Date: 'ZodDate',
+//     Set: 'ZodSet',
+//     Nullable: 'ZodNullable',
+//     Optional: 'ZodOptional',
+//     Record: 'ZodRecord',
+//     // Object: 'ZodObject',
+//   },
+//   // ========================================================================
+//   wrapInContainer: function (obj: any) {
+//     return v.object(obj);
+//   },
+//   isOwnContainer: function (obj: any) {
+//     const isValibotObjectType =
+//       obj?.expects === 'Object' && obj?.entries != null;
+//     return isValibotObjectType;
+//   },
+//   isOwnType: function (obj: any) {
+//     const isValibotType = obj?.async != null && obj?._run != null;
+//     return isValibotType;
+//   },
+//   jsonSchemaFrom: function (validationLibSchema: any) {
+//     // FIXME: needs to add option to de-ref json
+//     return toJsonSchema(validationLibSchema, { force: true });
+//   },
+// };
